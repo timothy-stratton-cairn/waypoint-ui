14f9a35168cb4296cc714c264c23eca63810fef3
Wesley Anderson <wes@rb-sns.com>
2024-03-11 08:52:03 -0500

    Back Button, fixed typo

--- src/main/java/com/cairn/ui/model/Protocol_step_admin.java
+++ src/main/java/com/cairn/ui/model/Protocol_step_admin.java
@@ -42,7 +42,7 @@ public class Protocol_step_admin {
             "    {\"stepId\": 1, \"stepName\": \"Step 1 Gather Data\", \"details\": \"Details for Step 1\"},\n" +
             "    {\"stepId\": 2, \"stepName\": \"Step 2 Run Analysis \", \"details\": \"Details for Step 2\"},\n" +
             "    {\"stepId\": 3, \"stepName\": \"Step 3 Craft Recommendation\", \"details\": \"Details for Step 3\"},\n" +
-            "    {\"stepId\": 4, \"stepName\": \"Step 4 Share Education\", \"details\": \"Details for Step 4\"},\n" +
+            "    {\"stepId\": 4, \"stepName\": \"Step 4 Share Education\", \"details\": \"Details for Step 4\"}\n" +
             "  ]\n" +
             "}";
 
