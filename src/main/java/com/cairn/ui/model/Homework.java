f332b8b495bace35b38d1f4db71c28b903e1f401
Rob Broadhead <rob@rb-sns.com>
2024-04-30 09:54:12 -0500

    Minor button/display changes and fix to display homework list.

--- src/main/java/com/cairn/ui/model/Homework.java
+++ src/main/java/com/cairn/ui/model/Homework.java
@@ -27,59 +27,6 @@ public class Homework {
 	public void setName(String name) {
 		this.name = name;
 	}
-	static public ArrayList<Homework> getList() {
-		ArrayList<Homework> results = new ArrayList();
-		
-		String data = "{\n"
-				+ "    \"protocols\": [\n"
-				+ "        {\n"
-				+ "            \"id\": 1,\n"
-				+ "            \"name\": \"New Client\"\n"
-				+ "        },\n"
-				+ "        {\n"
-				+ "            \"id\": 2,\n"
-				+ "            \"name\": \"House Purchase\"\n"
-				+ "        },\n"
-				+ "        {\n"
-				+ "            \"id\": 3,\n"
-				+ "            \"name\": \"Job Change/Retirement\"\n"
-				+ "        },\n"
-				+ "        {\n"
-				+ "            \"id\": 4,\n"
-				+ "            \"name\": \"Marriage\"\n"
-				+ "        },\n"
-				+ "        {\n"
-				+ "            \"id\": 5,\n"
-				+ "            \"name\": \"Baby\"\n"
-				+ "        }\n"
-				+ "    ],\n"
-				+ "    \"numOfProtocols\": 5\n"
-				+ "}";
-		
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            JsonNode jsonNode = objectMapper.readTree(data);
-            JsonNode prots = jsonNode.get("protocols");
-            // Iterate through the array elements
-            Homework entry = null;
-            if (prots.isArray()) {
-                for (JsonNode element : prots) {
-                    // Access and print array elements
-                	if (element != null) {
-                		entry = new Homework();
-                		entry.setName(element.get("name").asText());
-                		entry.setId(Integer.valueOf(element.get("id").toString()));
-                		results.add(entry);
-                	}
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-		return results;
-	}
 	public void setDescription(String Description) {
 		this.description = Description;
 	}
